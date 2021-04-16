b16b47d990ff4c967cd291e89b87a8c4e9441880
Ram N <ramram_43210@yahoo.com>
2021-04-16 10:28:33 +0530

    1

---
+++ Java_2021/SpringBoot_SetResponse_StatusCode/SpringBootDemo/src/main/java/com/ram/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.ram.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ram.model.Employee;
+
+@RestController
+public class EmployeeController
+{
+	@RequestMapping(value = "/employees/{employeeId}", method = RequestMethod.GET)
+	public ResponseEntity<Employee> getEmployee(@PathVariable int employeeId)
+	{
+		if (employeeId < 10)
+		{
+			Employee employee = new Employee();
+			employee.setId(employeeId);
+			employee.setName("Peter");
+			employee.setAge(34);
+			employee.setSalary(30000);
+			return new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		}
+		else
+		{
+			return new ResponseEntity<Employee>(HttpStatus.BAD_REQUEST);
+		}
+	}
+}
