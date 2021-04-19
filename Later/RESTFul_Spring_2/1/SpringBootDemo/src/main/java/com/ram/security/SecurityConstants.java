d7b81f21b2279c1044d812fa8a96235e2e8fa5f0
Ram N <ramram_43210@yahoo.com>
2021-04-19 14:43:59 +0530

    1

---
+++ Later/RESTFul_Spring_2/1/SpringBootDemo/src/main/java/com/ram/security/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.ram.security;
+
+public class SecurityConstants
+{
+	public static final long EXPIRATION_TIME=864000000; //10 Days
+	public static final String TOKEN_PREFIX="Bearer ";
+	public static final String HEADER_STRING="Authorization";
+	public static final String SIGNUP_URL="/users"; 
+	public static final String TOKEN_SECRET="kan89kjljtrt"; 
+
+}
