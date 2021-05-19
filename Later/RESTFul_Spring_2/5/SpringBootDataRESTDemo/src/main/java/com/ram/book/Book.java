7fbdde7becb290757c8df781fafe4db045ec918c
Ram N <ramram_43210@yahoo.com>
2021-05-19 10:52:10 +0530

    1

---
+++ Later/RESTFul_Spring_2/5/SpringBootDataRESTDemo/src/main/java/com/ram/book/Book.java
@@ -0,0 +1,49 @@
+package com.ram.book;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Book
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String title;
+
+	private String description;
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+}
