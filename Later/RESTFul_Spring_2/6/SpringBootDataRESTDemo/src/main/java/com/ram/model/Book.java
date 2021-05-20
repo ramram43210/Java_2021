38a4d1d12392626fa3dd2ad74315b214c1fa884f
Ram N <ramram_43210@yahoo.com>
2021-05-20 13:00:42 +0530

    1

---
+++ Later/RESTFul_Spring_2/6/SpringBootDataRESTDemo/src/main/java/com/ram/model/Book.java
@@ -0,0 +1,69 @@
+package com.ram.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String title;
+
+	private String description;
+
+	@ManyToOne
+	@JoinColumn(name = "book_category_id")
+	private BookCategory bookCategory;
+
+	public Book()
+	{
+
+	}
+
+	public int getId()
+	{
+		return id;
+	}
+
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+	public BookCategory getBookCategory()
+	{
+		return bookCategory;
+	}
+
+	public void setBookCategory(BookCategory bookCategory)
+	{
+		this.bookCategory = bookCategory;
+	}
+}
