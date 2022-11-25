fa598fd3992323da6750b21a6d042cdd3e4df4e9
Ram N <ramram_43210@yahoo.com>
2022-11-25 10:07:32 +0530

    1

---
+++ Java_2021/Springboot_Kafka_Producer_Consumer/SpringbootKafkaConsumer/SpringbootKafkaConsumerDemo/src/test/java/com/ram/KafkaProducerDemo/KafkaProducerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ram.KafkaProducerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
