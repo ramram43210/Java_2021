f4c3a543c09766443d3de1f282431b0d093795dc
Ram N <ramram_43210@yahoo.com>
2022-11-28 14:46:55 +0530

    1

---
+++ Java_2021/SpringbootKafkaProducer_ec2/SpringbootKafkaProducerDemo/src/main/java/com/ram/KafkaProducerDemo/Producer/KafkaProducerImpl.java
@@ -0,0 +1,18 @@
+package com.ram.KafkaProducerDemo.Producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducerImpl
+{
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+	private final static String TOPIC_NAME = "AnimalTopic";
+
+	public void sendMessage(String animalName)
+	{
+		kafkaTemplate.send(TOPIC_NAME, animalName);
+	}
+}
